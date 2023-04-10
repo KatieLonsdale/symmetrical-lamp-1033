b98360a7dbec54a19378322f6909ad9e81ddad09
Katie Lonsdale <katie952@gmail.com>
2023-04-10 11:09:13 -0600

    test: customer_names method

--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Supermarket, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  describe 'instance methods' do
+    before :each do
+      test_data
+    end
+    describe 'customer_names' do
+      it 'returns an array of unique customers who have shopped there' do
+        expect(@supermarket_1.customer_names.sort).to eq(['Katie', 'Max'])
+        expect(@supermarket_2.customer_names.sort).to eq(['Lily', 'Max', 'Sara'])
+        expect(@supermarket_3.customer_names).to eq([])
+      end
+    end
+  end
 end
