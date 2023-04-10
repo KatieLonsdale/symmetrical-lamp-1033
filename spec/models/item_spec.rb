9f9ff76f9013c61a0b4d77e3c15ec54d51dcd2ed
Katie Lonsdale <katie952@gmail.com>
2023-04-10 10:24:50 -0600

    test: add test for supermarket_name

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Item, type: :model do
     it { should have_many :customer_items }
     it { should have_many(:customers).through(:customer_items) }
   end
+
+  describe 'instance methods' do
+    before :each do
+      test_data
+    end
+    describe 'supermarket_name' do
+      it 'returns the name of the supermarket the item belongs to' do
+        expect(@item_1.supermarket_name).to eq('King Soopers')
+        expect(@item_2.supermarket_name).to eq('King Soopers')
+        expect(@item_4.supermarket_name).to eq('Safeway')
+      end
+    end
+  end
 end
