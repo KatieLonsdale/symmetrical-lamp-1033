e0b3a1cfd2c876ee53a4900856e909f25c5b88a3
Katie Lonsdale <katie952@gmail.com>
2023-04-10 10:28:53 -0600

    test: customers_purchased_count method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -18,5 +18,12 @@ RSpec.describe Item, type: :model do
         expect(@item_4.supermarket_name).to eq('Safeway')
       end
     end
+    describe 'customers_purchased_count' do
+      it 'returns the number of customers who purchased the item' do
+        expect(@item_1.customers_purchased_count).to eq(1)
+        expect(@item_2.customers_purchased_count).to eq(2)
+        expect(@item_6.customers_purchased_count).to eq(0)
+      end
+    end
   end
 end
