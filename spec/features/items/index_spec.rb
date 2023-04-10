0ccd150b9c2c916734e3fcdffadc2fac8ed75fd7
Katie Lonsdale <katie952@gmail.com>
2023-04-10 10:36:20 -0600

    test: add within blocks

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -8,15 +8,19 @@ RSpec.describe 'Items index page' do
 
   describe 'As a visitor, when I visit the items index page' do
     it "shows me a list of all items and their info" do
-      expect(page).to have_content("Name: #{@item_1.name}")
-      expect(page).to have_content("Price: #{@item_1.price}")
-      expect(page).to have_content("Supermarket: #{@item_1.supermarket_name}")
-      expect(page).to have_content("Purchased by #{@item_1.customers_purchased_count} Customers")
+      within("#item-#{@item_1.id}") do
+        expect(page).to have_content("Name: #{@item_1.name}")
+        expect(page).to have_content("Price: #{@item_1.price}")
+        expect(page).to have_content("Supermarket: #{@item_1.supermarket_name}")
+        expect(page).to have_content("Purchased by #{@item_1.customers_purchased_count} Customers")
+      end
 
-      expect(page).to have_content("Name: #{@item_4.name}")
-      expect(page).to have_content("Price: #{@item_4.price}")
-      expect(page).to have_content("Supermarket: #{@supermarket_2.name}")
-      expect(page).to have_content("Purchased by #{@item_4.customers_purchased_count} Customers")
+      within("#item-#{@item_4.id}") do
+        expect(page).to have_content("Name: #{@item_4.name}")
+        expect(page).to have_content("Price: #{@item_4.price}")
+        expect(page).to have_content("Supermarket: #{@supermarket_2.name}")
+        expect(page).to have_content("Purchased by #{@item_4.customers_purchased_count} Customers")
+      end
     end
   end
 end
